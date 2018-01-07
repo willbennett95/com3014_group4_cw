87adc467f8e4b33a80239163601b2877d9febaf2
Ivanova E Miss (UG - Computer Science) <ei00052@surrey.ac.uk>
2018-01-07 19:55:22 +0000

    upload audio file to directory
    Uploads the chosen file in the AudioFile folder which is above the project.
    ../com3014_group4_cw/Music/AudioFile/

--- Music/src/main/java/com/group4/music/config/WebConfig.java
+++ Music/src/main/java/com/group4/music/config/WebConfig.java
@@ -3,17 +3,13 @@ package com.group4.music.config;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
@@ -27,7 +23,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  *
- * @author bogdan
+ * @author Emi
  */
 @Configuration
 @EnableWebMvc
